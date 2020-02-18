8fc81ae0746cf146b1328ed5386f907db7b09862
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-18 21:14:28 +0200

    manual-generate > dev: about

---
+++ packages/manual-generate/about/generate.js
@@ -0,0 +1,11 @@
+const generate = {
+    render: 'react',
+    packages: [
+        '@plurid/plurid-data',
+        '@plurid/plurid-react',
+    ],
+    target: './plurid',
+};
+
+
+export default generate;
