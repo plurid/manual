e73829a72621b8f54589222010e56544b54951aa
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-18 20:14:42 +0200

    manual-react > fixture: package

---
+++ fixtures/manual-react-typescript/src/App-single.tsx
@@ -0,0 +1,26 @@
+import React, {
+    // useState,
+    // useEffect,
+} from 'react';
+
+import ManualApp, {
+} from '@plurid/manual-react';
+
+// import Page1 from './containers/Page1';
+// import Page2 from './containers/Page2';
+
+
+
+const App = () => {
+
+
+    return (
+        <div>
+            <ManualApp
+            />
+        </div>
+    );
+}
+
+
+export default App;
