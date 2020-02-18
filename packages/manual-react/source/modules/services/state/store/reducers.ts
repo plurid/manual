04abd87cb4de518beaffe09b5075e29658f548be
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-18 20:18:43 +0200

    manual-react > dev: state

--- packages/manual-react/source/modules/services/state/store/reducers.ts
+++ packages/manual-react/source/modules/services/state/store/reducers.ts
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 
 import * as configuration from '../modules/configuration';
 import * as shortcuts from '../modules/shortcuts';
-import * as space from '../modules/space';
 import * as themes from '../modules/themes';
 import * as ui from '../modules/ui';
 
@@ -11,7 +10,6 @@ import * as ui from '../modules/ui';
 const rootReducer = combineReducers({
     configuration: configuration.reducer,
     shortcuts: shortcuts.reducer,
-    space: space.reducer,
     themes: themes.reducer,
     ui: ui.reducer,
 });
