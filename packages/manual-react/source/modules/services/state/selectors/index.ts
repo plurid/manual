3d64e807ad86d3a898cf0ee4cfce99c8ae80af22
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-18 20:19:47 +0200

    manual-react > dev: state

--- packages/manual-react/source/modules/services/state/selectors/index.ts
+++ packages/manual-react/source/modules/services/state/selectors/index.ts
@@ -1,11 +1,9 @@
-import configuration from './configuration';
 import themes from './themes';
 import ui from './ui';
 
 
 
 export default {
-    configuration,
     themes,
     ui,
 };
--- packages/manual-react/source/modules/services/state/store/reducers.ts
+++ packages/manual-react/source/modules/services/state/store/reducers.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 
-import * as configuration from '../modules/configuration';
 import * as shortcuts from '../modules/shortcuts';
 import * as themes from '../modules/themes';
 import * as ui from '../modules/ui';
@@ -8,7 +7,6 @@ import * as ui from '../modules/ui';
 
 
 const rootReducer = combineReducers({
-    configuration: configuration.reducer,
     shortcuts: shortcuts.reducer,
     themes: themes.reducer,
     ui: ui.reducer,
