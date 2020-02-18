18488c60ad18ea3081b8e4e9c7950b7d9f57833c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-18 20:16:25 +0200

    manual-react > dev: state

--- packages/manual-react/source/modules/services/state/actions/index.ts
+++ packages/manual-react/source/modules/services/state/actions/index.ts
@@ -1,5 +1,4 @@
 import * as configuration from '../modules/configuration';
-import * as data from '../modules/data';
 import * as shortcuts from '../modules/shortcuts';
 import * as space from '../modules/space';
 import * as themes from '../modules/themes';
@@ -9,7 +8,6 @@ import * as ui from '../modules/ui';
 
 export default {
     configuration: configuration.actions,
-    data: data.actions,
     shortcuts: shortcuts.actions,
     space: space.actions,
     themes: themes.actions,
