375582cfc2fb1ef51b6110609d308b8d6aa43ebf
Jan Cavel <caveljan@gmail.com>
2020-02-18 20:14:42 +0200

    manual-react > fixture: package

---
+++ fixtures/manual-react-typescript/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+// import App from './App';
+import App from './App-single';
+
+import * as serviceWorker from './serviceWorker';
+
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root'),
+);
+
+serviceWorker.unregister();
