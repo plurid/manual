5864517bdc51f5035e91c9ea61de657acd1d97b2
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-19 15:12:48 +0200

    manual-react > dev: ManualElement

--- packages/manual-react/source/modules/components/ManualElement/styled.ts
+++ packages/manual-react/source/modules/components/ManualElement/styled.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 
 
 export const StyledManualElement = styled.div`
+    margin: 30px;
+    font-family: Ubuntu, -apple-system, BlinkMacSystemFont, Roboto,
+        'Open Sans', 'Helvetica Neue', 'Lucida Sans', sans-serif;
+    user-select: text;
 `;
