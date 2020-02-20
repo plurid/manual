0bdb487c3ada2530b28d29bedb4a5d49c8249e9e
Jan Cavel <caveljan@gmail.com>
2020-02-20 19:43:40 +0200

    manual-react > fixture: App

--- fixtures/manual-react-typescript/src/App.tsx
+++ fixtures/manual-react-typescript/src/App.tsx
@@ -3,18 +3,12 @@ import React from 'react';
 import ManualApp, {
     ManualModule,
     ManualMetadata,
-    // ManualData,
-    // ManualElementKind,
+    ManualData,
 } from '@plurid/manual-react';
-
-// import {
-//     JSONOutput,
-// } from 'typedoc';
 
 import pluridReactData from './testdata/data/@plurid/plurid-react-small.json';
 
 
-// console.log(ManualElementKind);
 
 const App = () => {
     const modules: ManualModule[] = [
@@ -24,31 +18,20 @@ const App = () => {
         },
     ];
 
+    const data: ManualData[] = [
+        pluridReactData,
+    ];
+
     const metadata: ManualMetadata = {
         name: '@plurid/plurid-react',
     };
-
-    // const data: JSONOutput.ProjectReflection[] = [
-    //     pluridReactData,
-    // ];
-
-    // const data: ManualData.ProjectReflection[] = [
-    //     pluridReactData,
-    // ];
-
-    // const a = 128;
-
-    // if (a === ManualElementKind.Class) {
-    //     console.log('ffooo');
-    // }
 
     return (
         <div>
             <ManualApp
                 modules={modules}
+                data={data}
                 metadata={metadata}
-                data={[pluridReactData]}
-                // data={data}
             />
         </div>
     );
